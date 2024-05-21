d1061418d3508d3e4ed9d8e3631ec6b40195f447
KostiaFed <91784684+KostiaFed@users.noreply.github.com>
2024-05-21 12:52:45 +0300

    chore: generate device views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root :to => redirect("books")
+  root "books#index"
 
   resources :books do
     collection do
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -22,5 +22,7 @@ FactoryBot.define do
   factory :user do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
+    email { Faker::Internet.email(name: "#{first_name} #{last_name}", separators: %w(. _ -), domain: 'gmail.com') }
+    password { Faker::Internet.password }
   end
 end
