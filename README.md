75c770c45323abfefa33b38d112b4bf0df331482
=kostiafed <=kostiantyn.fed@gmail.com>
2024-06-03 16:51:21 +0300

    feat: add alerts and registration

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Run ```sudo apt install imagemagick libvips```, ```bundle```, ```rails db:drop db:create db:migrate``` and ```rails s``` in project folder in console to start project.
 
 For testing and development ```rails db:seed```
+
+```mailcatcher``` to test mailer.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,6 @@ Rails.application.configure do
   end
 
   config.active_storage.service = :local
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_support.disallowed_deprecation = :raise
   config.active_support.disallowed_deprecation_warnings = []
@@ -31,5 +29,10 @@ Rails.application.configure do
   config.active_job.verbose_enqueue_logs = true
   config.assets.quiet = true
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: "localhost", port: 1025}
 end
