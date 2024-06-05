c4cdd8caac892d5e64c3cbcaefa6be0cccb467e1
=kostiafed <=kostiantyn.fed@gmail.com>
2024-06-05 15:45:26 +0300

    feat: add create new book route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @pagy, @books = pagy_countless(Book.all, items: 8)
 
@@ -12,7 +14,25 @@ class BooksController < ApplicationController
     @book = resource
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    if current_user.books << Book.new(book_params)
+      redirect_to book_path(current_user.books.last.id)
+    else
+      render "new"
+    end
+  end
+
   def resource
     Book.find(params[:id])
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :descrition, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
   }
 
   root "books#index"
 
-  resources :books
+  resources :books, only: [:index, :show, :new, :create]
 end
