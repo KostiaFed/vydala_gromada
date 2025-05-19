d097ef4f24040410b1065b45cf392812981012cc
Kostiantyn Fedorenko <kostiantyn.fedorenko@uitop.design>
2025-05-19 20:35:42 +0300

    chore: delete stimulus controller_generator

--- lib/generators/stimulus/controller/controller_generator.rb
+++
@@ -1,9 +0,0 @@
-require 'rails/generators/stimulus/controller_generator'
-
-module Stimulus
-  module Generators
-    class ControllerGenerator < Rails::Generators::Stimulus::ControllerGenerator
-      def update_manifest; end
-    end
-  end
-end
